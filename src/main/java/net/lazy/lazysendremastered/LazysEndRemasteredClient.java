4cda87a7f59f173670a1a31937d2d61cbe525717
PeppercodesYT <projecttoon@yahoo.com>
2025-12-15 18:50:05 -0500

    Initial commit

---
+++ src/main/java/net/lazy/lazysendremastered/LazysEndRemasteredClient.java
@@ -0,0 +1,8 @@
+package net.lazy.lazysendremastered;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LazysEndRemasteredClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {}
+}
