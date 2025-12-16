6ac3c4d57c642ad9707e54b324d1318b1466e5bc
PeppercodesYT <projecttoon@yahoo.com>
2025-12-15 20:45:57 -0500

    added Nether Soul

--- src/main/java/net/lazy/lazysendremastered/LazysEndRemastered.java
+++ src/main/java/net/lazy/lazysendremastered/LazysEndRemastered.java
@@ -2,6 +2,7 @@ package net.lazy.lazysendremastered;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.lazy.lazysendremastered.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +12,6 @@ public class LazysEndRemastered implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/lazy/lazysendremastered/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.lazy.lazysendremastered.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.lazy.lazysendremastered.LazysEndRemastered;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item PEPPERS_ITEM_TEST = registerItem("nether_soul", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(LazysEndRemastered.MOD_ID,"nether_soul")))));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(LazysEndRemastered.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        LazysEndRemastered.LOGGER.info("Registering Mod Items for " + LazysEndRemastered.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(PEPPERS_ITEM_TEST);
+        });
+    }
+}
